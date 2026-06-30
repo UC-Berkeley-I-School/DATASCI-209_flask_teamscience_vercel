bfd695171b8387aa7564de0c125c2122c5e7c546
t3resabui <teresabui27@yahoo.com>
2026-06-30 00:15:23 -0700

    added image to app

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def w209():
     file='about9.jpg'
-    return render_template('w209.html',file=file)
+    file2='term_grants_60_prj_period.png'
+    file3='dollar_breakdown.png'
+    return render_template('w209.html',file=file,file2=file2, file3=file3)
 
 if __name__ == '__main__':
     app.run()
