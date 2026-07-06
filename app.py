de9ea786f89801e2f298e115ffe6da7eb9b16899
t3resabui <teresabui27@yahoo.com>
2026-07-05 17:05:09 -0700

    app edits

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def w209():
-    file='about9.jpg'
+    file='nih_grant.png'
     file2='term_grants_60_prj_period.png'
     file3='dollar_breakdown.png'
-    return render_template('w209.html',file=file,file2=file2, file3=file3)
+    file4='region_termin_count.png'
+    file5='region_termin_rates.png'
+    return render_template('w209.html',file=file,file2=file2, 
+                           file3=file3,file4=file4, file5=file5)
 
 if __name__ == '__main__':
     app.run()
